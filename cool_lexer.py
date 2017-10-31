4b7eea2f3112a4789b23bb5c2f6cceae8d9e5934
Aljan Idris <A.Idris.1@warwick.ac.uk>
2017-10-31 09:57:23 +0000

    partial parser impl

---
+++ cool_lexer.py
@@ -0,0 +1,95 @@
+class LexError(Exception):
+    pass
+
+
+def initialise_patterns():
+    # orderedDict, so considers in order of insertion. add token_patterns in order of precedence.
+    token_patterns = collections.OrderedDict()
+
+    # case insensitive keywords
+    case_insensitive_keywords = ["class", "else", "fi", "if", "inherits", "in", "isvoid", "let",
+                                 "loop", "pool", "then", "while", "case", "esac",  "new", "of", "not"]
+    for kw in case_insensitive_keywords:
+        regex = re.compile(kw, re.IGNORECASE)
+        token_patterns[kw] = regex
+
+    # case sensitive keywords. first letter must be lower case, rest insensitive
+    case_sensitive_keywords = ["false", "true"]
+    for kw in case_sensitive_keywords:
+        # craft correct pattern from kw e.g. f(a|A)(l|L)(s|S)(e|E)
+        adjusted_kw = kw[0] + ''.join(["({0}|{1})".format(letter, letter.upper()) for letter in kw[1:]])
+        regex = re.compile(adjusted_kw)  # encoding as string-escape makes it evaluated as raw string
+        token_patterns[kw] = regex
+
+    # add recognizers for symbols
+    symbols = [";", ",", ":", "{", "}", "(", ")", "<-", "@", ".", "=>", "+", "-", "~", "*", "/", "<=", "<", "="]
+    # can group by precedence e.g. addop, mulop, etc. also, consider 'full' string matching,
+    # since = will match for <= and =>.
+    # maybe just solve w/ ordering? or do "^{0}$".format(pattern)
+    for symbol in symbols:
+        regex = re.compile(re.escape(symbol))
+        token_patterns[symbol] = regex
+
+    # integers
+    regex = re.compile(r"[0-9]+")
+    token_patterns["integer"] = regex
+
+    # special notation: self and SELF_TYPE
+    regex = re.compile(r"self")
+    token_patterns["self"] = regex
+
+    regex = re.compile(r"SELF_TYPE")
+    token_patterns["self_type"] = regex
+
+    # type identifier
+    regex = re.compile(r"[A-Z][A-Za-z0-9_]*")
+    token_patterns["type_id"] = regex
+
+    # object identifier
+    regex = re.compile(r"[a-z][A-Za-z0-9_]*")
+    token_patterns["object_id"] = regex
+
+    # strings
+    regex = re.compile(r'"(.*?)"', re.VERBOSE)
+    token_patterns["string"] = regex
+
+    # whitespace
+    regex = re.compile(r"\s")
+    token_patterns["whitespace"] = regex
+
+    return token_patterns
+
+
+# initialise patterns
+patterns = initialise_patterns()
+
+# remove
+def lex(lexer):
+    while True:
+        # use shlex lexeme splitter, it will split alphanumeric (with _), and individual punctuation symbols.
+        # need to slightly modify so that it matches multichar cool symbols as tokens (e.g. => as => not =,>)
+        lexeme = lexer.get_token()
+
+        while lexeme.count('"') == 1:
+            # if there is an unmatched quote, append shlex tokens until the lexeme has a matched quote
+            lexeme += lexer.get_token()
+
+        # print("<{0}>".format(lexeme), end=', ')
+        if lexeme == "eof":
+            break
+        match_pattern(lexeme, lexer)
+
+def next_word(lexer):
+    # candidate string, switch w/ lexeme types. pass lexeme type?
+    # list, precompiled regexes. get string token from shlex then pass it thru, returnign lex error if necessary else
+    # returning an encoding of a token types
+    lexeme = lexer.get_token()
+    # if there is an unmatched quote, append shlex tokens until the lexeme has a matched quote
+    while lexeme.count('"') == 1:
+        lexeme += lexer.get_token()
+    if lexeme == "eof":
+        return Token("eof", "")
+    return match_pattern(lexeme, lexer)
+
+
+Token = namedtuple('Token', 'name val')
